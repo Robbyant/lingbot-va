d0cbc68951315a2d88a452232cb348c4bb7fb0a3
Lau-JW <3110263305@qq.com>
2026-04-10 22:29:20 +0800

    Enable_wandb_by_default_for_arms
    Made-with:

--- wan_va/configs/va_arms_train_cfg.py
+++ wan_va/configs/va_arms_train_cfg.py
@@ -14,8 +14,8 @@ va_arms_train_cfg.empty_emb_path = os.path.join(va_arms_train_cfg.dataset_path, 
 # Set this to your downloaded lingbot-va-base path on the training machine.
 va_arms_train_cfg.wan22_pretrained_model_name_or_path = "/root/checkpoints/lingbot-va-base"
 
-# Default to off: server environments often don't have WANDB_* configured.
-va_arms_train_cfg.enable_wandb = False
+# Enable by default; training will auto-disable if WANDB_* env vars are missing.
+va_arms_train_cfg.enable_wandb = True
 # Single-GPU default: avoid multiprocessing CUDA issues when loading .pth files.
 va_arms_train_cfg.load_worker = 0
 va_arms_train_cfg.save_interval = 200
